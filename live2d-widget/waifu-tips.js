4589c400fb6cdc92abb69036d63ea931449e7865
crystal302 <realzombie462@gmail.com>
2020-12-14 06:57:09 +0800

    Site updated: 2020-12-14 06:57:08

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -113,6 +113,7 @@ function loadWidget(config) {
 			else if (now > 19 && now <= 21) text = "晚上好，今天过得怎么样？";
 			else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，晚安～", "深夜时要爱护眼睛呀！"];
 			else text = "你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？";
+text=text+"如果你看不见我的话，请多等一会儿哦~"
 		} else if (document.referrer !== "") {
 			const referrer = new URL(document.referrer),
 				domain = referrer.hostname.split(".")[1];
